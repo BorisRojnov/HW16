d920a1e27ec48eb59400294c5f2621240dc6498f
BorisRojnov <BorisRojnov@yandex.by>
2024-09-01 17:57:40 +0300

    homework complete

--- Task2/src/Main.java
+++ Task2/src/Main.java
@@ -1,8 +1,22 @@
+import java.util.Scanner;
+
 public class Main {
+    static final String VALID_TEXT = "([A-z]+\\s)";
+    static final String PUNCTUATION_MARKS = "[.,!?;:\"\"'']";
     public static void main(String[] args) {
         //todo  код
+        System.out.println("Введите английский текст: ");
+        String text = new Scanner(System.in).nextLine();
+        System.out.println(separateLine(text));
     }
-
     //todo метод
-
+public static String separateLine(String textEdit){
+       String noMarks = textEdit.replaceAll(PUNCTUATION_MARKS, " ");
+       String[] split = noMarks.split("\\s+");
+       String result = "";
+       for(int i = 0; i < split.length; i++){
+           result += split[i] + "\n";
+       }
+       return result;
+    }
 }
